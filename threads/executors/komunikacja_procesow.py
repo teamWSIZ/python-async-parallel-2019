c4b6a84191f8ff02428105589eac501dd320543c
Piotr Marecki <pm@aimachina.io>
2020-01-25 10:28:45 -0800

    wykorzystnie managera (dziala na windows)

--- threads/executors/komunikacja_procesow.py
+++ threads/executors/komunikacja_procesow.py
@@ -1,22 +1,24 @@
 from concurrent.futures.process import ProcessPoolExecutor
 from time import sleep
-from typing import List
+from typing import List, Dict
 import multiprocessing as mp
 
 
-def render_scene(arg, scena: List[int]):
+def render_scene(arg, scena: List[int], mapa: Dict):
     """
     """
     print(f'start task [{arg}] scena:{scena, id(scena)}')
     sleep(0.3)
-    print(f'koniec task [{arg}]')
+    print(f'koniec task [{arg}], value={mapa["xxx"]}')
+    mapa['xxx'] = arg
     return arg * arg
 
 
-man = mp.Manager()
-mapa = man.dict()  # dict shared between processes
 
 if __name__ == '__main__':
+    man = mp.Manager()
+    mapa = man.dict()  # dict shared between processes
+    mapa['xxx'] = -1
     executor = ProcessPoolExecutor()
     for i in range(5):
-        executor.submit(render_scene, i, [])
+        executor.submit(render_scene, i, [], mapa)
