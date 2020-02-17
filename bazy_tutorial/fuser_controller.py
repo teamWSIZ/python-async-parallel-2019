b2f96323ebc05cd7365c6813c09d9e146f14f718
Piotr Marecki <pm@aimachina.io>
2020-02-17 08:39:50 +0100

    cleanup

--- bazy_tutorial/fuser_controller.py
+++ bazy_tutorial/fuser_controller.py
@@ -1,11 +1,5 @@
-import json
-import time
-from typing import Dict
-import threading
-
 from flask import Flask, jsonify, request
-
-from bazy_tutorial.a import get_all_users, get_all_users_with_name_prefix, FakeUser, as_dict
+from bazy_tutorial.peewee_example import get_all_users, get_all_users_with_name_prefix, FakeUser, as_dict
 
 app = Flask(__name__)
 
@@ -32,24 +26,6 @@ def upsert_user():
     k = FakeUser(**data)
     k.save()
     return jsonify(as_dict(k))
-
-# # dostępna pod: http://localhost:5001/compute?a=10&b=0
-# @app.route('/compute')
-# def compute():
-#     a = int(request.args.get('a'))
-#     b = int(request.args.get('b'))
-#     print(f'request a={a}, thread:{threading.current_thread().name}')
-#     time.sleep(10.0)
-#     if b == 0:
-#         # teraz zwracamy komunikat o błędzie, oraz http error-code 400 (BAD_REQUEST)
-#         return jsonify({'comment': 'b==0, cannot divide'}), 400
-#     return jsonify({'sum': a + b, 'difference': a - b, 'division': a / b})
-#
-#
-# # dostępna pod: http://localhost:5001/welcome/roadrunner/suffix/nice%20to%20meet%20you
-# @app.route('/welcome/<username>/suffix/<message>')
-# def welcome(username, message):
-#     return jsonify({'comment': f'Hello {username}, {message}!'})
 
 
 app.run(host='localhost', port=5001, debug=None, load_dotenv=False)  # can skip all args
